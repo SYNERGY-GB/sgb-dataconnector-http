dc0f53dadb03ff8376a985e1f2f2329d766805cb
Saul Hernandez <sauldhernandez@gmail.com>
2015-10-14 11:54:39 -0430

    Now adds a body automatically if none is present

--- dist/sgb-dataconnector-http.js
+++ dist/sgb-dataconnector-http.js
@@ -4,8 +4,11 @@
 angular.module('sgb-dataconnector-http', ['megazord'])
     .factory('sgb-dataconnector-http', ['$http', function ($http) {
         return {
-            getData: function (params) {
+            getData: function (params, event) {
                 //Params must have the same syntax as the $http config object
+                if (!params.data && event && event.params) {
+                    params.data = event.params;
+                }
                 return $http(params);
             }
         } ;
