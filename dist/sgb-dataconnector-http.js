90c3182f11e5d95ab137d9dc620e61ebe82a2d8b
Luis Miglietti <luismiglietti@gmail.com>
2015-10-21 17:48:42 -0430

    forgot to include lodash

--- dist/sgb-dataconnector-http.js
+++ dist/sgb-dataconnector-http.js
@@ -2,7 +2,7 @@
 'use strict';
 
 angular.module('sgb-dataconnector-http', ['megazord'])
-    .factory('sgb-dataconnector-http', ['$http', function ($http) {
+    .factory('sgb-dataconnector-http', ['$http','lodash', function ($http,_) {
         return {
             getData: function (params, event) {
                 var httpParams = _.cloneDeep(params);
